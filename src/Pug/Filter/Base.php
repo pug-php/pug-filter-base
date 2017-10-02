6ef1fae8bb586842f2dcf1f97a1bb28559a972e2
KyleKatarn <kylekatarnls@gmail.com>
2017-10-02 22:23:50 +0200

    Use separated directories

--- src/Pug/Filter/Base.php
+++ src/Pug/Filter/Base.php
@@ -1,3 +1,3 @@
 <?php
 
-include_once __DIR__ . '/BasePugPhp' . (class_exists('Pug\\AbstractFilter') ? 3 : 2) . '.php';
+include_once __DIR__ . '/../../Pug' . (class_exists('Pug\\AbstractFilter') ? 3 : 2) . '/Filter/Base.php';
